f83b8bdeef94a6c39654fefdd1c5f88753b6df81
arieri <ariericyron@gmail.com>
2022-06-08 17:34:03 +0300

    commit tree 133516cd71bac6bf11745f1c4458866903847261

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+
+function writeCards(name,event){
+ let thanksCard=[]
+ for(let i=0;i<name.length;i++){
+    thanksCard.push(`Thank you, ${name[i]}, for the wonderful ${event} gift!`)
+ }
+ return thanksCard  
+}
+function countDown(count){
+while(count>0){
+    console.log(count)
+    count -=1
+}
+console.log(count)
+}
